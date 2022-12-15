0e5117d80381169cb5ae651e8407caca96dbe837
mautnerK <mautner@net.hr>
2022-12-15 18:39:24 +0100

    Refactoring

---
+++ BestTimes/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using BestTimes.Data;
+using BestTimes.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BestTimes.Controllers
+{
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            AdminLoginInfo adminLogin = new AdminLoginInfo();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(AdminLoginInfo adminLogin)
+        {
+            BestTimesContext context = new BestTimesContext();
+            var status = context.AdminLoginInfo.Where(m => m.Username == adminLogin.Username && m.Password == adminLogin.Password).FirstOrDefault();
+            if (status == null)
+            {
+                ViewBag.LoginStatus = 0;
+            }
+            else
+            {
+                return RedirectToAction("SuccessPage", "PendingBestTimes");
+            }
+            return View();
+        }
+
+    }
+}
--- BestTimes/Controllers/PendingBestTimesController.cs
+++ BestTimes/Controllers/PendingBestTimesController.cs
@@ -21,30 +21,6 @@ namespace BestTimes.Controllers
         {
             this.service = service;
         }
-
-        [HttpGet]
-        public IActionResult Index()
-        {
-            AdminLoginInfo adminLogin = new AdminLoginInfo();
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult Index(AdminLoginInfo adminLogin)
-        {
-            BestTimesContext context = new BestTimesContext();
-            var status = context.AdminLoginInfo.Where(m => m.Username == adminLogin.Username && m.Password == adminLogin.Password).FirstOrDefault();
-            if (status == null)
-            {
-                ViewBag.LoginStatus = 0;
-            }
-            else
-            {
-                return RedirectToAction("SuccessPage", "PendingBestTimes");
-            }
-            return View();
-        }
-
         public async Task<IActionResult> SuccessPage()
         {
             ViewModel mymodel = new ViewModel();
